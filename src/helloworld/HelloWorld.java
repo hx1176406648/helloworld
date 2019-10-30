f1fa120f75acc9239cdb0703f030af3f1e22f693
hx1176406648 <52735697+hx1176406648@users.noreply.github.com>
2019-10-30 11:09:12 +0800

    test_branch

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Hello World Ëæ±ã¸ÄÒ»¸Ä");
+        System.out.println("Hello World 666666666666");
     }
 }
