553cb9b202ac6024c20056fa3f1e2ab9b62d75c4
hx1176406648 <1176406648@qq.com>
2019-10-30 10:34:51 +0800

    test_change

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Hello World my git");
+        System.out.println("Hello World Ëæ±ã¸ÄÒ»¸Ä");
     }
 }
